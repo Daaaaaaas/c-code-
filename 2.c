0a5418ceac51c67f02f89bd1087e886adee16b2e
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-12 18:46:53 +0530

    Add files via upload
    2

---
+++ 2.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+
+ void main(){
+    
+   float a,b,sum;
+ printf("give first number:-");
+ scanf("%f",&a);
+ printf("give second number:-");
+ scanf("%f",&b);
+   sum=a+b;
+printf("Adittiom of two numbers is:-");
+printf("%f",sum);
+}
