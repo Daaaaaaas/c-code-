3bd18fb9a31f12b5f38d28bc57a60da1a65f485b
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-12 18:55:02 +0530

    Add files via upload
    4

---
+++ 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+  float a,b,c,average;
+  printf("give first number:-");
+ scanf("%f",&a);
+  printf("give second number:-");
+ scanf("%f",&b);
+ printf("give third number:-");
+ scanf("%f",&c);
+
+average=(a+b+c)/3.0;
+printf("average of three numbers is:-");
+printf("%.2f",average);
+
+}
