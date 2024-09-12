62e86fefe56190e110301be9c8dee963bc333567
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-12 19:26:48 +0530

    Add files via upload
    7

---
+++ 7.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+ int Hours,Minutes,Seconds,n;
+ printf("give seconds to convert in HH:MM:SS format:-");
+ scanf("%d",&n);
+  
+  Hours=n/3600;
+  Minutes=(n%3600)/60;
+ Seconds=(n%3600)%60;
+
+ printf("Given seconds into HH:MM:SS format--> "); 
+ printf("%d",Hours);
+ printf(":");
+ printf("%d",Minutes); 
+ printf(":");
+ printf("%d",Seconds);
+}
