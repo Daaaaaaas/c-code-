89ae68e8b50aa2e5bc4410f0d0c22ebd7913f897
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-12 19:51:59 +0530

    Add files via upload
    8

---
+++ 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+ int Year,Week,Days,n;
+ printf("give Days convert into year,week & days format :-");
+ scanf("%d",&n);
+  
+  Year=n/365;
+  Week=(n%365)/7;
+ Days=(n%365)%7;
+
+ printf("Given Days into  year,week & days format--> "); 
+ printf("%d",Year);
+ printf(" Year ");
+ printf("%d",Week); 
+ printf(" Week  ");
+ printf("%d",Days);
+ printf(" Days");
+}
