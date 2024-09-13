fc48eaccd61017659de8c1fb7834249798a5a622
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-13 19:11:20 +0530

    Add files via upload
    11

---
+++ 11.c
@@ -0,0 +1,19 @@
+//11)Check whether given number is positive or negative.
+
+#include <stdio.h>
+
+void main(){
+  int n;
+ 
+printf("give number-->");
+scanf("%d",&n);
+
+if(n>0){
+ printf("Number is positive");
+}
+else{
+ printf("Number is negative");
+
+}
+ 
+}
