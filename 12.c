aa9153493bd9b4d982455383602da75e62087cf3
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-13 19:17:42 +0530

    Add files via upload
    12

---
+++ 12.c
@@ -0,0 +1,17 @@
+//12) Check whether the given number is odd or even.
+
+#include <stdio.h>
+ 
+void main(){
+  int n;
+  printf("Give Number-->");
+  scanf("%d",&n);
+  if(n%2==0){
+printf("Given number is even");
+}
+else{
+printf("Given number is odd");
+
+}
+
+}
