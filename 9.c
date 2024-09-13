70414cadc6b31952872843412ea5b90c60af154b
Daaaaaaas <jayjeetbbaraiya@gmail.com>
2024-09-13 18:35:58 +0530

    Add files via upload
    9

---
+++ 9.c
@@ -0,0 +1,12 @@
+//print Given feet into inches.
+
+#include<stdio.h>
+void main (){
+  int Feet,inches;
+  printf("Give total Feet:-");
+  scanf("%d",&Feet);
+  
+  inches=Feet*12;
+printf("Given Feet into inches-->");
+printf("%d",inches);
+}
